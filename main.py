ff3657d3938de3855a28deedb41044af93fab487
Luke Sorenson <lasoren@me.com>
2016-03-20 17:00:46 -0400

    First attempt at computing points earned so far

--- main.py
+++ main.py
@@ -5,17 +5,24 @@ import time
 
 from datetime import datetime
 
+BASE_POINTS = 10
+ROUND_MULTIPLE = 2
+NUM_ROUNDS = 6
+
 FORECAST_LOC = "fivethirtyeight_ncaa_forecasts.csv"
 GENDER_KEY = "gender"
 FORECAST_DATE_KEY = "forecast_date"
 TEAM_ID_KEY = "team_id"
+ROUND_1_WIN_KEY = "rd1_win"
+ROUND_2_WIN_KEY = "rd2_win"
 LEAGUE = "mens"
 
 def teams_to_dict():
     import teams2016
     teams_dict = {}
     for team in teams2016.TEAMS:
-        teams_dict[team.replace(" ", "")] = 0
+        # Start at -1 so that only picks are counted. The name appearing once means nothing.
+        teams_dict[team.replace(" ", "")] = -1
     num_teams = len(teams_dict)
     if num_teams != 64:
         raise ValueError('The number of entered teams is ' + str(num_teams))
@@ -54,6 +61,8 @@ def process_538_data():
     gender_idx = 0
     forecast_date_idx = 0
     team_id_idx = 0
+    round_1_win_idx = 0
+    round_2_win_idx = 0
     first_row = rows[0]
     for i in range(len(first_row)):
         if first_row[i] == GENDER_KEY:
@@ -62,11 +71,17 @@ def process_538_data():
             forecast_date_idx = i
         elif first_row[i] == TEAM_ID_KEY:
             team_id_idx = i
-
+        elif first_row[i] == ROUND_1_WIN_KEY:
+            round_1_win_idx = i
+        elif first_row[i] == ROUND_2_WIN_KEY:
+            round_2_win_idx = i
     # Filter out old data and women's data.
     latest_date = None
     for i in range(len(rows) - 1, -1, -1):
         if rows[i][gender_idx] != LEAGUE:
+            rows.remove(rows[i])
+        # Remove teams that did not win the play in.
+        elif float(rows[i][round_1_win_idx]) == 0.0:
             rows.remove(rows[i])
         else:
             # Find the date on each row and determine the last date supplied.
@@ -78,15 +93,33 @@ def process_538_data():
     for i in range(len(rows) - 1, -1, -1):
         if rows[i][forecast_date_idx] != latest_date:
             rows.remove(rows[i])
-    return rows, team_id_idx
+    return rows, team_id_idx, round_2_win_idx
 
-prob_rows, team_id_idx = process_538_data()
+prob_rows, team_id_idx, round_1_win_idx = process_538_data()
 teams_dict, id_map = teams_to_dict()
 
+text = textract.process('bracket_images/luke.pdf', method='pdfminer')
 
-# text = textract.process('bracket_images/luke.pdf', method='pdfminer')
-#
-# print(count_picks(teams_dict, text))
-# print(teams_dict)
+print(id_map)
+for row in prob_rows:
+    print row
 
+count_picks(teams_dict, text)
+print(teams_dict)
+
+points = 0
+current_point_award = BASE_POINTS
+for i in range(NUM_ROUNDS):
+    current_round_idx = round_1_win_idx + i
+    for row in prob_rows:
+        # If the team won and the pick was made.
+        team_name = id_map[int(row[team_id_idx])]
+        if float(row[current_round_idx]) == 1.0 and teams_dict[team_name] > 0:
+            points += current_point_award
+            # Remove a selected win from that team.
+            teams_dict[team_name] -= 1
+    # Double the points at the end of the round.
+    current_point_award *= ROUND_MULTIPLE
+
+print(points)
 
